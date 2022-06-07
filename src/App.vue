febb68ac72f3fde47f84de3c5e2911a247e85887
Giovanni Ciotta <giociotta@gmail.com>
2022-06-07 18:03:53 +0200

    ex finito

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <myNewcomponent/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import myNewcomponent from './components/myNewcomponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    myNewcomponent
+
   }
 }
 </script>
--- src/components/myNewcomponent.vue
+++ src/components/myNewcomponent.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h2>La mia prima app con Vue CLI</h2>
    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'myNewcomponent',
+  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
+h2 {
   margin: 40px 0 0;
+  color: #42b983;
 }
 ul {
   list-style-type: none;
